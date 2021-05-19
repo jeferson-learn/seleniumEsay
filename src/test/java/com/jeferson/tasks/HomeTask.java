0e788767dc33fa69ad78d2c2afb077b9d03ab03a
Jeferson Lopes Eugenio <jefyx1@hotmail.com>
2021-05-19 18:07:55 -0300

    Modifiquei nome do classe e um acao clica para entrar

--- src/test/java/com/jeferson/tasks/HomeTask.java
+++ src/test/java/com/jeferson/tasks/HomeTask.java
@@ -3,18 +3,19 @@ package com.jeferson.tasks;
 import com.jeferson.appobjects.HomeApp;
 import org.openqa.selenium.WebDriver;
 
-public class SimpleFormDemoTask {
+public class HomeTask {
 
     private final WebDriver driver;
     private final HomeApp homeApp;
 
-    public SimpleFormDemoTask(WebDriver driver){
+    public HomeTask(WebDriver driver){
         this.driver = driver;
         homeApp = new HomeApp(driver);
     }
 
-    public void fulanoParteAcoes(){
+    public void entrarSimpleFormDemo(){
         homeApp.abaInputFormsButton().click();
+        homeApp.menuSimpleFormDemoButton().click();
     }
 
 }
