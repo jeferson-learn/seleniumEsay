45be1f00f9b77eeba8acdcfb4a9b5a6926d3fa05
Jeferson Lopes Eugenio <jefyx1@hotmail.com>
2021-05-19 18:09:27 -0300

    Modifiquei nome do classe, adicionei metodo para teste entrar 'simple form demo' e coloquei barra

--- src/test/java/com/jeferson/testcases/InputFormsTest.java
+++ src/test/java/com/jeferson/testcases/InputFormsTest.java
@@ -6,23 +6,23 @@ import com.jeferson.framework.tools.Report;
 import com.jeferson.framework.tools.Screenshot;
 import com.jeferson.framework.utils.FileOperationsAndProperties;
 import com.jeferson.framework.webdrivers.DriverManager;
-import com.jeferson.tasks.Task;
+import com.jeferson.tasks.HomeTask;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
 import static com.jeferson.framework.tools.Report.extentTest;
 
-public class InputForms extends BaseTest {
+public class InputFormsTest extends BaseTest {
 
     private final WebDriver driver = DriverManager.getDriver();
-    private final Task task = new Task(driver);
+    private final HomeTask homeTask = new HomeTask(driver);
 
     @Test
     public void simpleFormDemo(){
         Report.createTest("Teste 'Input Forms");
         DriverManager.getDriver().get(FileOperationsAndProperties.getPropertiesData("url", "seleniumEasy"));
         extentTest.log(Status.INFO, "Abriu a navegador no 'SELENIUM EASY'", Screenshot.screenshotBase64(driver));
-        task.fulanoParteAcoes();
-        extentTest.log(Status.INFO, "Finalmente de testes", Screenshot.screenshotBase64(driver));
+        homeTask.entrarSimpleFormDemo();
+//        extentTest.log(Status.INFO, "Finalmente de testes", Screenshot.screenshotBase64(driver));
     }
 }
