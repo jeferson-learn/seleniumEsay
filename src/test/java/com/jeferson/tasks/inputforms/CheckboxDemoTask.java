c15472de54103a3f6dece0cfdc43281256a45821
Jeferson Lopes Eugenio <jefyx1@hotmail.com>
2021-05-26 12:33:33 -0300

    Clica botao para selecionar tudo checkbox e outro opcoes clicar cada checkbox

--- src/test/java/com/jeferson/tasks/inputforms/CheckboxDemoTask.java
+++ src/test/java/com/jeferson/tasks/inputforms/CheckboxDemoTask.java
@@ -1,6 +1,7 @@
 package com.jeferson.tasks.inputforms;
 
 import com.jeferson.appobjects.inputforms.CheckboxDemoApp;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 
 public class CheckboxDemoTask {
@@ -14,6 +15,8 @@ public class CheckboxDemoTask {
 
     public void singleCheckboxDemo() {
         checkboxDemoApp.clickOnThisCheckBox().click();
+        String messageSuccess = checkboxDemoApp.messageSuccess().getText();
+        Assertions.assertEquals("Success - Check box is checked", messageSuccess);
     }
 
     public void multipleCheckboxDemo() {
@@ -21,5 +24,46 @@ public class CheckboxDemoTask {
         checkboxDemoApp.option2().click();
         checkboxDemoApp.option3().click();
         checkboxDemoApp.option4().click();
+    }
+
+    public void multipleCheckboxDemoCheckAll(){
+        checkboxDemoApp.checkAllButton().click();
+    }
+
+    public void chooseSelectCheckboxDemo(int num1) {
+        String [] value = new String[checkboxDemoApp.checkboxs().size()];
+        for (int i = 0; i<checkboxDemoApp.checkboxs().size(); i++) {
+            value[i] = checkboxDemoApp.checkboxs().get(i).getText();
+            if (num1 == i) {
+                checkboxDemoApp.checkboxs().get(i).click();
+            }
+        }
+    }
+    public void chooseSelectCheckboxDemo(int num1, int num2) {
+        String [] value = new String[checkboxDemoApp.checkboxs().size()];
+        for (int i = 0; i<checkboxDemoApp.checkboxs().size(); i++) {
+            value[i] = checkboxDemoApp.checkboxs().get(i).getText();
+            if (num1 == i || num2 == i) {
+                checkboxDemoApp.checkboxs().get(i).click();
+            }
+        }
+    }
+    public void chooseSelectCheckboxDemo(int num1, int num2, int num3) {
+        String [] value = new String[checkboxDemoApp.checkboxs().size()];
+        for (int i = 0; i<checkboxDemoApp.checkboxs().size(); i++) {
+            value[i] = checkboxDemoApp.checkboxs().get(i).getText();
+            if (num1 == i || num2 == i || num3 == i) {
+                checkboxDemoApp.checkboxs().get(i).click();
+            }
+        }
+    }
+    public void chooseSelectCheckboxDemo(int num1, int num2, int num3, int num4) {
+        String [] value = new String[checkboxDemoApp.checkboxs().size()];
+        for (int i = 0; i<checkboxDemoApp.checkboxs().size(); i++) {
+            value[i] = checkboxDemoApp.checkboxs().get(i).getText();
+            if (num1 == i || num2 == i || num3 == i || num4 == i) {
+                checkboxDemoApp.checkboxs().get(i).click();
+            }
+        }
     }
 }
