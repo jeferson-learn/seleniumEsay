55fa889d1ebe7ec546097668fc5651c462a1ca1c
Jeferson Lopes Eugenio <jefyx1@hotmail.com>
2021-06-01 11:04:08 -0300

    Adicionei os elementos link abas e menus na pagina home

--- src/test/java/com/jeferson/appobjects/HomeApp.java
+++ src/test/java/com/jeferson/appobjects/HomeApp.java
@@ -71,4 +71,23 @@ public class HomeApp {
     public WebElement menuTableDataDownloadButton() {
         return driver.findElement(By.linkText("Table Data Download"));
     }
+    public WebElement abaProgressBarsButton() { return  driver.findElement(By.linkText("Progress Bars")); }
+    public WebElement menuJQueryDownloadProgressBarsButton() { return  driver.findElement(By.linkText("JQuery Download Progress bars")); }
+    public WebElement menuBootstrapProgressBarButton() { return  driver.findElement(By.linkText("Bootstrap Progress bar")); }
+    public WebElement menuDragDropSlidersButton() { return  driver.findElement(By.linkText("Drag & Drop Sliders")); }
+    public WebElement abaAlertsModalsButton() { return  driver.findElement(By.linkText("Alerts & Modals")); }
+    public WebElement menuBootstrapAlertsButton() { return  driver.findElement(By.linkText("Bootstrap Alerts")); }
+    public WebElement menuBootstrapModalsButton() { return  driver.findElement(By.linkText("Bootstrap Modals")); }
+    public WebElement menuWindowPopupModalButton() { return  driver.findElement(By.linkText("Window Popup Modal")); }
+    public WebElement menuProgressBarModalButton() { return  driver.findElement(By.linkText("Progress Bar Modal")); }
+    public WebElement menuJavascriptAlertsButton() { return  driver.findElement(By.linkText("Javascript Alerts")); }
+    public WebElement menuFileDownloadButton() { return  driver.findElement(By.linkText("File Download")); }
+    public WebElement abaListBoxButton() { return  driver.findElement(By.linkText("List Box")); }
+    public WebElement menuBootstrapListBoxButton() { return  driver.findElement(By.linkText("Bootstrap List Box")); }
+    public WebElement menuJQueryListBoxButton() { return  driver.findElement(By.linkText("JQuery List Box")); }
+    public WebElement menuDataListFilterButton() { return  driver.findElement(By.linkText("Data List Filter")); }
+    public WebElement abaOthersButton() { return  driver.findElement(By.linkText("Others")); }
+    public WebElement menuDragAndDropButton() { return  driver.findElement(By.linkText("Drag and Drop")); }
+    public WebElement menuDynamicDataLoadingButton() { return  driver.findElement(By.linkText("Dynamic Data Loading")); }
+    public WebElement menuChartsDemoButton() { return  driver.findElement(By.linkText("Charts Demo")); }
 }
