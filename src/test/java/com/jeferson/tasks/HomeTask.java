081aeb34d15b9583bb4875eee4079fa909e2cfe7
Jeferson Lopes Eugenio <jefyx1@hotmail.com>
2021-06-02 14:14:26 -0300

    Adicionei um metodo fecha popup dentro outros metodos

--- src/test/java/com/jeferson/tasks/HomeTask.java
+++ src/test/java/com/jeferson/tasks/HomeTask.java
@@ -14,129 +14,153 @@ public class HomeTask {
     }
 
     public void fecharPopup() {
-        homeApp.fechaPopup().click();
+        if (homeApp.fechaPopup().isDisplayed()){
+            homeApp.fechaPopup().click();
+            System.out.println("fechou popup");
+        }
     }
     public void entrarSimpleFormDemo(){
+        fecharPopup();
         homeApp.abaInputFormsButton().click();
         homeApp.menuSimpleFormDemoButton().click();
     }
     public void entrarCheckboxDemo(){
+        fecharPopup();
         homeApp.abaInputFormsButton().click();
         homeApp.menuCheckboxDemoButton().click();
     }
     public void entrarRadioButtonsDemo(){
-        if (homeApp.fechaPopup().isDisplayed()){
-            homeApp.fechaPopup().click();
-            System.out.println("fechou popup");
-        }
+        fecharPopup();
         homeApp.abaInputFormsButton().click();
         homeApp.menuRadioButtonsDemoButton().click();
     }
     public void entrarSelectDropdownList(){
+        fecharPopup();
         homeApp.abaInputFormsButton().click();
         homeApp.menuSelectDropdownListButton().click();
     }
     public void entrarInputFormSubmit(){
+        fecharPopup();
         homeApp.abaInputFormsButton().click();
         homeApp.menuInputFormSubmitButton().click();
     }
     public void entrarAjaxFormSubmit(){
+        fecharPopup();
         homeApp.abaInputFormsButton().click();
         homeApp.menuAjaxFormSubmitButton().click();
     }
     public void entrarJQuerySelectDropdown(){
-        if (homeApp.fechaPopup().isDisplayed()){
-            homeApp.fechaPopup().click();
-            System.out.println("fechou popup");
-        }
+        fecharPopup();
         homeApp.abaInputFormsButton().click();
         homeApp.menuJQuerySelectDropdownButton().click();
     }
     public void entrarBootstrapDatePicker() {
+        fecharPopup();
         homeApp.abaDatePickersButton().click();
         homeApp.menuBootstrapDatePickerButton().click();
     }
     public void entrarJQueryDatePicker() {
+        fecharPopup();
         homeApp.abaDatePickersButton().click();
         homeApp.menuJQueryDatePickerButton().click();
     }
     public void entrarTablePagination() {
+        fecharPopup();
         homeApp.abaTableButton().click();
         homeApp.menuTablePaginationButton().click();
     }
     public void entrarTableDataSearch() {
+        fecharPopup();
         homeApp.abaTableButton().click();
         homeApp.menuTableDataSearchButton().click();
     }
     public void entrarTableFilter() {
+        fecharPopup();
         homeApp.abaTableButton().click();
         homeApp.menuTableFilterButton().click();
     }
     public void entrarTableSortSearch() {
+        fecharPopup();
         homeApp.abaTableButton().click();
         homeApp.menuTableSortSearchButton().click();
     }
     public void entrarTableDataDownload() {
+        fecharPopup();
         homeApp.abaTableButton().click();
         homeApp.menuTableDataDownloadButton().click();
     }
     public void entrarJQueryDownloadProgressBars() {
+        fecharPopup();
         homeApp.abaProgressBarsButton().click();
         homeApp.menuJQueryDownloadProgressBarsButton().click();
     }
     public void entrarBootstrapProgressBar() {
+        fecharPopup();
         homeApp.abaProgressBarsButton().click();
         homeApp.menuBootstrapProgressBarButton().click();
     }
     public void entrarDragDropSliders() {
+        fecharPopup();
         homeApp.abaProgressBarsButton().click();
         homeApp.menuDragDropSlidersButton().click();
     }
     public void entrarBootstrapAlerts() {
+        fecharPopup();
         homeApp.abaAlertsModalsButton().click();
         homeApp.menuBootstrapAlertsButton().click();
     }
     public void entrarBootstrapModals() {
+        fecharPopup();
         homeApp.abaAlertsModalsButton().click();
         homeApp.menuBootstrapModalsButton().click();
     }
     public void entrarWindowPopup() {
+        fecharPopup();
         homeApp.abaAlertsModalsButton().click();
         homeApp.menuWindowPopupModalButton().click();
     }
     public void entrarProgressBarModal() {
+        fecharPopup();
         homeApp.abaAlertsModalsButton().click();
         homeApp.menuProgressBarModalButton().click();
     }
     public void entrarJavascriptAlerts() {
+        fecharPopup();
         homeApp.abaAlertsModalsButton().click();
         homeApp.menuJavascriptAlertsButton().click();
     }
     public void entrarFileDownload() {
+        fecharPopup();
         homeApp.abaAlertsModalsButton().click();
         homeApp.menuFileDownloadButton().click();
     }
     public void entrarBootstrapListBox() {
+        fecharPopup();
         homeApp.abaListBoxButton().click();
         homeApp.menuBootstrapListBoxButton().click();
     }
     public void entrarJQueryListBox() {
+        fecharPopup();
         homeApp.abaListBoxButton().click();
         homeApp.menuJQueryListBoxButton().click();
     }
     public void entrarDataListFilter() {
+        fecharPopup();
         homeApp.abaListBoxButton().click();
         homeApp.menuDataListFilterButton().click();
     }
     public void entrarDragAndDrop() {
+        fecharPopup();
         homeApp.abaOthersButton().click();
         homeApp.menuDragAndDropButton().click();
     }
     public void entrarDynamicDataLoading() {
+        fecharPopup();
         homeApp.abaOthersButton().click();
         homeApp.menuDynamicDataLoadingButton().click();
     }
     public void entrarChartsDemo() {
+        fecharPopup();
         homeApp.abaOthersButton().click();
         homeApp.menuChartsDemoButton().click();
     }
