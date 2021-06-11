28cc56cf5c8baa77cb95167e2cc662d4bede8f22
Jeferson Lopes Eugenio <jefyx1@hotmail.com>
2021-06-11 12:46:07 -0300

    Criei classe data download test, executa teste baixar todas listas escolhe um tipo arquivo, escolhe ordem tipo e escolhe um tipo arquivo para baixar e pesquisa no campo, escolhe um tipo arquivo para baixar

---
+++ src/test/java/com/jeferson/testcases/table/TableDataDownload.java
@@ -0,0 +1,49 @@
+package com.jeferson.testcases.table;
+
+import com.aventstack.extentreports.Status;
+import com.jeferson.framework.supports.BaseTest;
+import com.jeferson.framework.tools.Report;
+import com.jeferson.framework.tools.Screenshot;
+import com.jeferson.framework.utils.FileOperationsAndProperties;
+import com.jeferson.framework.webdrivers.DriverManager;
+import com.jeferson.tasks.HomeTask;
+import com.jeferson.tasks.table.TableDataDownloadTask;
+import org.junit.jupiter.api.Test;
+
+import static com.jeferson.framework.tools.Report.extentTest;
+
+public class TableDataDownload extends BaseTest {
+    private final HomeTask homeTask = new HomeTask(driver);
+    private final TableDataDownloadTask tableDataDownloadTask = new TableDataDownloadTask(driver);
+
+    @Test
+    public void baixarTodosListaEscolheTipoArquivo(){
+        String copy = "copy", csv = "csv", excel = "excel", pdf = "pdf", print = "print";
+        Report.createTest("Teste 'Table search field'");
+        DriverManager.getDriver().get(FileOperationsAndProperties.getPropertiesData("url", "seleniumEasy"));
+        extentTest.log(Status.INFO, "Abriu a navegador no 'SELENIUM EASY'", Screenshot.screenshotBase64(driver));
+        homeTask.entrarTableDataDownload();
+        tableDataDownloadTask.clickTypeDownload(excel);
+    }
+
+    @Test
+    public void escolheOrdemBaixarTodosListaEscolheTipoArquivo(){
+        String name = "name", position = "position", office = "office", age = "age", startDate = "start date", salary = "salary";
+        String copy = "copy", csv = "csv", excel = "excel", pdf = "pdf", print = "print";
+        Report.createTest("Teste 'Table search field'");
+        DriverManager.getDriver().get(FileOperationsAndProperties.getPropertiesData("url", "seleniumEasy"));
+        extentTest.log(Status.INFO, "Abriu a navegador no 'SELENIUM EASY'", Screenshot.screenshotBase64(driver));
+        homeTask.entrarTableDataDownload();
+        tableDataDownloadTask.sortTable(age, copy);
+    }
+
+    @Test
+    public void pesquisaCampoBaixarEscolheTipoArquivo(){
+        String copy = "copy", csv = "csv", excel = "excel", pdf = "pdf", print = "print";
+        Report.createTest("Teste 'Table search field'");
+        DriverManager.getDriver().get(FileOperationsAndProperties.getPropertiesData("url", "seleniumEasy"));
+        extentTest.log(Status.INFO, "Abriu a navegador no 'SELENIUM EASY'", Screenshot.screenshotBase64(driver));
+        homeTask.entrarTableDataDownload();
+        tableDataDownloadTask.searchDownload("Lon", copy);
+    }
+}
