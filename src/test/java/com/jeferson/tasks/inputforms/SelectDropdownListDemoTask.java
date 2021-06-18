6e877ef3baf7475f2b92900f1815311208b3ced4
Jeferson Lopes Eugenio <jefyx1@hotmail.com>
2021-06-18 16:49:09 -0300

    Tirei codigo que ficou bagunca, coloquei melhor

--- src/test/java/com/jeferson/tasks/inputforms/SelectDropdownListDemoTask.java
+++ src/test/java/com/jeferson/tasks/inputforms/SelectDropdownListDemoTask.java
@@ -2,9 +2,7 @@ package com.jeferson.tasks.inputforms;
 
 import com.jeferson.appobjects.inputforms.SelectDropdownListDemoApp;
 import org.hamcrest.MatcherAssert;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.Actions;
 
 import static org.hamcrest.core.StringContains.containsString;
 
@@ -24,41 +22,24 @@ public class SelectDropdownListDemoTask {
         MatcherAssert.assertThat(messagem, containsString(text));
     }
 
-    public void multiSelectListDemo(int num, String text) {
-        if (1 == num) {
-            selectDropdownListDemoApp.multiSelectListDemoSelect().selectByValue(text);
-            selectDropdownListDemoApp.firstSelectedButton().click();
-        }
-        if (2 <= num && 8 >= num) {
-
-        }
-        selectDropdownListDemoApp.multiSelectListDemoSelect().selectByValue("California");
-        selectDropdownListDemoApp.multiSelectListDemoSelect().selectByValue("New York");
-        selectDropdownListDemoApp.multiSelectListDemoSelect().selectByValue("Washington");
+    public void multiSelectListFirstSelect(String select) {
+        selectDropdownListDemoApp.multiSelectListDemoSelect().selectByVisibleText(select);
+        selectDropdownListDemoApp.firstSelectedButton().click();
     }
 
-    public void teste() throws InterruptedException {
+    public void multiSelectListGetAllSelect(String[] list) {
         String[] value = new String[selectDropdownListDemoApp.listValueSelect().size()];
-        int[] random = new int[2];
-        Actions actions = new Actions(driver);
         for (int i = 0; i < value.length; i++) {
             value[i] = selectDropdownListDemoApp.listValueSelect().get(i).getText();
         }
-        for (int i = 0; i < random.length; i++) {
-            int random_int = (int) Math.floor(Math.random() * (7 - 2 + 1) + 2);
-            System.out.println("sorteio: " + random_int);
-            random[i] = random_int;
-            System.out.println("array: " + random[i]);
-            actions.keyDown(Keys.LEFT_CONTROL)
-                    .click(selectDropdownListDemoApp.listValueSelect().get(random[i]))
-                    .keyUp(Keys.LEFT_CONTROL)
-                    .build()
-                    .perform();
-            Thread.sleep(2000);
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < value.length; j++) {
+                if (list[i].equals(value[j])) {
+                    String valor = list[i];
+                    selectDropdownListDemoApp.multiSelectListDemoSelect().selectByVisibleText(valor);
+                }
+            }
         }
         selectDropdownListDemoApp.getAllSelectedButton().click();
-        Thread.sleep(2000);
     }
-
-
 }
